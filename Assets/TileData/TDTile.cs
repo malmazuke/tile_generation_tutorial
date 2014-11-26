a4a008ffeb3ad1eb3e081407b3c58aee732b5408
Mark Feaver <mark.feaver@gmail.com>
2014-11-26 20:20:37 +1300

    Changed tiles to use Enums for type

--- Assets/TileData/TDTile.cs
+++ Assets/TileData/TDTile.cs
@@ -2,16 +2,13 @@
 
 public class TDTile {
 	
-	// TODO: Use enums
-	// 0: Ocean
-	// 1: Grass
-	// 2: Plains
-	// 3: Mountain
-	public const int TILE_OCEAN = 0;
-	public const int TILE_GRASSLAND = 1;
-	public const int TILE_PLAINS = 2;
-	public const int TILE_MOUNTAIN = 3;
+	enum TYPE {
+		OCEAN,
+		GRASSLAND,
+		PLAINS,
+		MOUNTAIN
+	}
 	
-	public int type = TDTile.TILE_OCEAN;
+	public TYPE type = TYPE.OCEAN;
 
 }
