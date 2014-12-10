0885eb346ea851852dd199d1fe77b431da184d48
Mark Feaver <mark.feaver@gmail.com>
2014-12-10 19:56:45 +1300

    Created Room object to store room info

--- Assets/TileMap_D/DTileMap.cs
+++ Assets/TileMap_D/DTileMap.cs
@@ -1,6 +1,15 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+
 public class DTileMap {
 
+	protected class DRoom {
+		public int left;
+		public int top;
+		public int width;
+		public int height;
+	}
+	
 	public enum TYPE {
 		BLANK,
 		FLOOR,
@@ -10,20 +19,32 @@ public class DTileMap {
 
 	int size_x;
 	int size_y;
-
+	
 	TYPE[,] map_data;
+	
+	List<DRoom> rooms;
 
 	public DTileMap(int size_x, int size_y) {
 		this.size_x = size_x;
 		this.size_y = size_y;
 
 		map_data = new TYPE[this.size_x, this.size_y];
+		
+		rooms = new List<DRoom>();
 
 		for (int i = 0; i < 10; i++) {
 			int r_sizeX = Random.Range(4, 8);
 			int r_sizeY = Random.Range(4, 8);
 
-			MakeRoom (Random.Range(0, size_x - r_sizeX), Random.Range(0, size_y - r_sizeY), r_sizeX, r_sizeY);
+			DRoom r = new DRoom();
+			r.left = Random.Range(0, size_x - r_sizeX);
+			r.top = Random.Range(0, size_y - r_sizeY);
+			r.width = r_sizeX;
+			r.height = r_sizeY;
+			
+			rooms.Add(r);
+			
+			MakeRoom(r);
 		}
 	}
 
@@ -31,14 +52,14 @@ public class DTileMap {
 		return map_data [x, y];
 	}
 
-	void MakeRoom(int left, int top, int width, int height) {
+	void MakeRoom(DRoom room) {
 
-		for (int x = 0; x < width; x++) {
-			for (int y = 0; y < height; y++) {
-				if (x == 0 || x == width - 1 || y == 0 || y == height - 1) {
-					map_data[left + x, top + y] = TYPE.WALL;
+		for (int x = 0; x < room.width; x++) {
+			for (int y = 0; y < room.height; y++) {
+				if (x == 0 || x == room.width - 1 || y == 0 || y == room.height - 1) {
+					map_data[room.left + x, room.top + y] = TYPE.WALL;
 				} else {
-				map_data[left + x, top + y] = TYPE.FLOOR;
+					map_data[room.left + x, room.top + y] = TYPE.FLOOR;
 				}
 			}
 		}
